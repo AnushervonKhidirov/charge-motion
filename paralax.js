d118eb02267a6db5d500f49775fa920343568180
DenzelHawking <denzelhawking@gmail.com>
2019-09-01 17:49:47 +0300

    paralax-js

---
+++ paralax.js
@@ -0,0 +1,14 @@
+var paralaxPhone = document.querySelector('.phone-paralax');
+var paralaxItem = document.querySelectorAll('.paralax');
+
+
+window.addEventListener('mousemove', function (event) {
+  var left = event.pageX / 50;
+  var top = event.pageY / 33;
+
+  for (var i = 0; i < paralaxItem.length; i++) {
+    paralaxItem[i].style.transform = 'translate(-' + left + 'px, -' + top + 'px)';
+  };
+
+  paralaxPhone.style.transform = 'translate(-' + left + 'px, -' + top + 'px) rotate(-30deg)';
+});
