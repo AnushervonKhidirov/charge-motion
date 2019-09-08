b2eec8cecd9261a2eb1b30d14619a847237815c1
DenzelHawking <denzelhawking@gmail.com>
2019-09-08 21:26:41 +0300

    add isPromission function and modified mousewheelSwitch

--- js/switch.js
+++ js/switch.js
@@ -2,40 +2,60 @@ var dotItem = document.querySelectorAll('.dot-item');
 var lengthItem = dotItem.length - 1;
 var activeDot = document.querySelectorAll('.active-dot');
 var menuItem = document.querySelectorAll('.menu-item');
+var scrolling = document.querySelector('.choose-third-page-menu')
 var isScrolling = true;
+var permission = true;
 var numberIndex = 0;
-
-var partners = document.querySelector('.partners')
 
 window.addEventListener('load', switching(0));
 
-window.onmousewheel = function (event) {
+window.onmousewheel = function () {
+  mousewheelSwitch(event);
+}
+
+function isPermission() {
+  scrolling.onmouseover = function () {
+    permission = false;
+    console.log(permission);
+  }
+
+  scrolling.onmouseout = function () {
+    permission = true;
+    console.log(permission);
+  }
+}
+
+function mousewheelSwitch(event) {
+  isPermission()
+
   var delta = event.deltaY;
 
-  if (isScrolling) {
-    if (delta < 0) {
-      if (numberIndex <= 0) {
-        numberIndex = lengthItem;
-      } else {
-        numberIndex--;
+  if (permission) {
+    if (isScrolling) {
+      if (delta < 0) {
+        if (numberIndex <= 0) {
+          numberIndex = lengthItem;
+        } else {
+          numberIndex--;
+        };
       };
-    };
 
-    if (delta > 0) {
-      if (numberIndex >= lengthItem) {
-        numberIndex = 0;
-      } else {
-        numberIndex++;
+      if (delta > 0) {
+        if (numberIndex >= lengthItem) {
+          numberIndex = 0;
+        } else {
+          numberIndex++;
+        };
       };
+
+      switching(numberIndex);
+
+      isScrolling = false;
+
+      setTimeout(function () {
+        isScrolling = true;
+      }, 1200);
     };
-
-    switching(numberIndex);
-
-    isScrolling = false;
-
-    setTimeout(function () {
-      isScrolling = true;
-    }, 1200);
   };
 };
 
